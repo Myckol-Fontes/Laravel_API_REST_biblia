321dfa74bc32d6363072ecaca16d58af760d7b0d
Myckol Fontes <myckol.ti@gmail.com>
2022-05-18 23:37:15 -0300

    CRUD Livro

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestamentoController;
+use App\Http\Controllers\LivroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,12 @@ Route::get('/testamento', [TestamentoController::class, 'index']);
 Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
 Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
 Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
+
+Route::post('/livro', [LivroController::class, 'store']);
+Route::get('/livro', [LivroController::class, 'index']);
+Route::get('/livro/{id}', [LivroController::class, 'show']);
+Route::put('/livro/{id}', [LivroController::class, 'update']);
+Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
